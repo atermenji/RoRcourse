e443497f0b65b0c1f4da290ac5afd9de3102ca7a
Artur Termenji <atermenji@gmail.com>
2013-01-18 01:16:36 +0200

    modify method now accepts classes instead of instances

--- task_1/task1.rb
+++ task_1/task1.rb
@@ -18,19 +18,19 @@ class TimeUtils
 end
 
 # Modify method (#method_name) from class (#cls_name)
-def modify(cls_name, method_name)
+def modify(cls, method_name)
 
-	cls_name.class.send :define_method, method_name do
+	cls.send :define_method, method_name do
   		return "Your \"#{method_name}\" method was hacked!!!11"
 	end
 
 end
 
-#=======================================
+#========================================
 
 time_utils = TimeUtils.new
 puts "Today is : #{time_utils.week_day}"
 
-modify(time_utils, "week_day")
+modify(TimeUtils, "week_day")
 
 puts "Modified : #{time_utils.week_day}"

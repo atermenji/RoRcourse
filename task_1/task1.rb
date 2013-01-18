100f6e0ac94986c09d499d0a9ae7819930c086b3
Artur Termenji <atermenji@gmail.com>
2013-01-18 11:40:00 +0200

    added checking for method existance

--- task_1/task1.rb
+++ task_1/task1.rb
@@ -19,6 +19,17 @@ end
 # Modify method (#method_name) from class (#cls)
 def modify(cls, method_name)
 
+	if cls.send :method_defined?, method_name
+		cls.send :define_method, method_name do
+	  		"Your \"#{method_name}\" method was hacked!!!11"
+		end
+	end
+
+end
+
+# Add method (#method_name) to class (#cls_name) or rewrite existing one
+def add(cls, method_name)
+
 	cls.send :define_method, method_name do
   		"Your \"#{method_name}\" method was hacked!!!11"
 	end

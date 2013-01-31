8261fb68cac0e3081c4731b89d9e7c9c6540697b
Artur Termenji <atermenji@gmail.com>
2013-01-31 14:39:49 +0200

    indentation change to 2 spaces (ruby guidelines)

--- task_3/task3.rb
+++ task_3/task3.rb
@@ -6,32 +6,32 @@ end
 
 class Car
 
-	attr_accessor :engine, :size, :turbo
+  attr_accessor :engine, :size, :turbo
 
-	def initialize(args = nil)
-		yield self if block_given?
+  def initialize(args = nil)
+    yield self if block_given?
 
-		args.each do |k, v|
-			no_such_attribute!(k) unless respond_to? k.to_sym
-  			instance_variable_set("@#{k}", v) unless v.nil?
-		end if args.is_a? Hash
-	end
+    args.each do |k, v|
+      no_such_attribute!(k) unless respond_to? k.to_sym
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end if args.is_a? Hash
+  end
 
-	def engine_info
-		attribute_undefined!(:engine) if @engine == nil
-		attribute_undefined!(:size) if @size == nil
-			
-		return "Turbo #{@engine} engine #{@size.to_f}" if @turbo
-		return "#{size.to_f} #{@engine.capitalize} engine"
-	end
+  def engine_info
+    attribute_undefined!(:engine) if @engine == nil
+    attribute_undefined!(:size) if @size == nil
+        
+    return "Turbo #{@engine} engine #{@size.to_f}" if @turbo
+    return "#{size.to_f} #{@engine.capitalize} engine"
+  end
 
-	def no_such_attribute!(attribute)
-		raise NoSuchAttributeException.new("#{attribute} param does not exist" )
-	end	
+  def no_such_attribute!(attribute)
+    raise NoSuchAttributeException.new("#{attribute} param does not exist" )
+  end 
 
-	def attribute_undefined!(attribute)
-		raise AttributeUndefinedException.new("#{attribute} is undefined")
-	end
+  def attribute_undefined!(attribute)
+    raise AttributeUndefinedException.new("#{attribute} is undefined")
+  end
 
 end
 
@@ -51,10 +51,10 @@ turbo_car = Car.new(engine: :diesel, size: 2, turbo: true)
 puts "Turbo car info : #{turbo_car.engine_info}"
 
 block_car = Car.new do |car|
-	car.engine = :diesel
-	car.size = 3
+  car.engine = :diesel
+  car.size = 3
 end
 puts "Init from block : #{block_car.engine_info}"
 
-# Car.new.engine_info		# => Engine is undefined (AttributeUndefinedException)
-# Car.new(ololo: true)		# => ololo param does not exist (NoSuchAttributeException)
+# Car.new.engine_info       # => Engine is undefined (AttributeUndefinedException)
+# Car.new(ololo: true)      # => ololo param does not exist (NoSuchAttributeException)
